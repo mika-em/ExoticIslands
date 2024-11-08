99207585bd06e11df26b97337ea701dd36e11670
Conrad Christian <cchristian5@se12l0325-03.ad.bcit.ca>
2024-11-08 11:38:43 -0800

    add project

---
+++ ExoticIslands/ExoticIslands/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ExoticIslands
+//
+//  Created by Conrad Christian on 2024-11-08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ExoticIslands/ExoticIslands/ExoticIslandsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExoticIslandsApp.swift
+//  ExoticIslands
+//
+//  Created by Conrad Christian on 2024-11-08.
+//
+
+import SwiftUI
+
+@main
+struct ExoticIslandsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
